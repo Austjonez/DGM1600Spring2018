a9c84524f4cb919a3f1ce22aa24efe3bca3539d6
Unknown <austjonez@gmail.com>
2018-01-18 11:09:17 -0700

    added hagrid. added vader. pushed gandolf off the cliff
    Removed gandolf

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -8,8 +8,13 @@ public class Animal : MonoBehaviour {
 	void Start () {
 		print("Hello Dave.");
 		print("One ring to rule them all");
+		print("yer a wizard, harry");
+		print("join me, and together we can rule the galaxy as father and son");
 			}
-	
+	void OnMouseDown()
+	{
+		print("fly you fools");
+	}
 	// Update is called once per frame
 	void Update () {
 		
