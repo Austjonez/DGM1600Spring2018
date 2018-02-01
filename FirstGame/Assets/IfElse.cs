5d4cf4e028b654d348d2efe0a1f064739639e494
Unknown <austjonez@gmail.com>
2018-02-01 11:17:42 -0700

    Added IF/Else statement

---
+++ FirstGame/Assets/IfElse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElse : MonoBehaviour {
+	public bool Key;
+	void Start () {
+		if (Key)
+		{
+			print("I have the key.");
+		}
+		else{
+			print("You shall not pass!");
+		}
+	}
+}
