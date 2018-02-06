e78c5116fef7aba3411fca8d5ebe6e4f7993c84f
Unknown <austjonez@gmail.com>
2018-02-06 11:23:19 -0700

    Added Logic operator and Scriptable Object

---
+++ FirstGame/Assets/Scripts/LogicOperators.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicOperators : MonoBehaviour {
+
+	public int DarthVader;
+	public int Obiwan;
+	public string LukeEnters;
+	void Start () {
+		if (DarthVader > Obiwan && LukeEnters == "True")
+		{
+			print("luke screams 'no!'");
+		}
+		if (DarthVader < Obiwan && LukeEnters != "True")
+		{
+			print("I have the high ground!");
+		}
+		if (DarthVader < Obiwan && LukeEnters == "True")
+		{
+			print("wait, what?");
+		}
+		if (DarthVader > Obiwan && LukeEnters != "True")
+		{
+			print("Now I am the master!");
+		}
+	}
+
+}
