3036d0878d6094afdb4e6edcbc4225fb6436b793
Unknown <austjonez@gmail.com>
2018-02-13 11:19:02 -0700

    Added Arrays and More Scriptable Objects

---
+++ FirstGame/Assets/Scripts/ArraysAndLoops.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ArraysAndLoops : MonoBehaviour {
+    public int[] HighScores;
+    public PowerUp[] Powerups;
+	public Player[] Players;
+    void Start () {
+        foreach (var player in Players) {
+            print (player.PlayerName);
+			print (player.Score);
+			print (player.MP);
+        }
+        foreach (var scores in HighScores) {
+            if (scores >= 1000) {
+                print (scores);
+            }
+        }
+
+    }
+}
