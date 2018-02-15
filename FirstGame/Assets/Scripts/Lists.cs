773f8b371df3e23a5343ca064d144cd701e3b628
Unknown <austjonez@gmail.com>
2018-02-15 11:18:59 -0700

    Added Lists

---
+++ FirstGame/Assets/Scripts/Lists.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lists : MonoBehaviour {
+
+	public Incredibles Incredibles;
+	public Text AddMember;
+	
+	void OnMouseDown()
+	{
+		Incredibles.FamilyList.Add(AddMember.text);
+	}
+}
