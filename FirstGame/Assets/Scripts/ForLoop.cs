189a0fcd8d30aa16f7f2caf1576225c3e9515897
Unknown <austjonez@gmail.com>
2018-02-27 11:16:04 -0700

    Added forLoops

---
+++ FirstGame/Assets/Scripts/ForLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour {
+
+	public string[] Keys;
+
+	void Start () {
+			// this is meh
+		for (int i = 0; i < Keys.Length; i++)	{
+			if (Keys[i] == "Add Ammo"){
+			print(Keys[i]);
+			}
+		}
+			//this is better
+		//foreach (var names in Keys)
+		//{
+		//	print(Keys);
+		//}
+	}
+}
