00073570d3a647745934a436dfb5c439fde6e1c2
Unknown <austjonez@gmail.com>
2018-03-06 11:15:53 -0700

    Added PowerUpReturn and FunctionsWithReturns

---
+++ FirstGame/Assets/Scripts/Functions/FunctionsWithReturns.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FunctionsWithReturns : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+		//does work
+		//returns nothing
+
+		//	MyInt = AddHealth();
+	//}
+
+	//public int MyInt = 50 + 50;
+
+	//public int AddHealth()
+	//{
+	//	return 50 + 50;
+	//}
+
+	public int Health;
+
+	public int AddHealth(int value)
+	{
+		print("Hello There");
+		//move character
+		//play an animation
+		return Health += value;
+	}
+
+	private void OnMouseDown()
+	{
+		Health = AddHealth(10);
+		Players.Add(NewPlayer("Bob"));
+	}
+
+	public List<string> Players;
+
+	public List<string> NewPlayers ()
+	{
+		return new List<string>();
+	}
+
+	public string NewPlayer(string name)
+	{
+		return name;
+	}
+
+}
