cd7d8df29ccca1cd12d845a63490e43133b348bb
Unknown <austjonez@gmail.com>
2018-04-03 11:14:36 -0600

    Character Controller Input Scriptable Object

--- FirstGame/Assets/Scripts/CharacterController/Move.cs
+++ FirstGame/Assets/Scripts/CharacterController/Move.cs
@@ -10,11 +10,15 @@ public float jumpSpeed = 8.0F;
 public float gravity = 20.0F;
 private Vector3 moveDirection = Vector3.zero;
 
+public InputBase InputX;
+public InputBase InputZ;
+public InputBase InputY;
+
 public void Movement(CharacterController controller, Transform transform){
  	if (controller.isGrounded) {
-		moveDirection.x = Input.GetAxis("Horizontal");
-		moveDirection.z = Input.GetAxis("Vertical");
-		moveDirection.y = 0;
+		moveDirection.x = InputX.SetFloat();
+		moveDirection.z = InputZ.SetFloat();
+		moveDirection.y = InputY.SetFloat();
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
             if (Input.GetButton("Jump"))
