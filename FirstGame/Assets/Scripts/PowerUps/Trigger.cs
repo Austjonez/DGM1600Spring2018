a136f05072748055fda1ca2a486b4a9fb671711b
Unknown <austjonez@gmail.com>
2018-04-10 11:21:54 -0600

    Changing move

--- FirstGame/Assets/Scripts/PowerUps/Trigger.cs
+++ FirstGame/Assets/Scripts/PowerUps/Trigger.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Trigger : MonoBehaviour {
 
 	public PowerUp PowerUpTransfer;
+	private Move 
 
 	private void OnTriggerEnter(Collider obj)
 	{
+		PrevMove = obj.GetComponent<MoveCharacter>().Move;
+
 		obj.GetComponent<MoveCharacter>().Move = PowerUpTransfer.Transfer();
+		
+		gameObject.SetActive(false);
+	}
+
+	void RestoreMove(Move OldMove)
+	{
+		
 	}
 }
