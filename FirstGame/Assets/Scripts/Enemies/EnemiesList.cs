857751e7bf4c4d8de59d885f8279d18a94ce643e
Unknown <austjonez@gmail.com>
2018-04-26 11:30:22 -0600

    Prefabs, For Each, And if else

---
+++ FirstGame/Assets/Scripts/Enemies/EnemiesList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesList : MonoBehaviour {
+
+	public string[] Enemies;
+
+	private void Start() {
+		foreach (string names in Enemies)
+		{
+			print("Enemies");
+		}
+	}
+}
---
+++ FirstGame/Assets/Scripts/Winning/Winning.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winning : MonoBehaviour {
+
+	public int Health = 1;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (Health == 1)
+		{
+			print ("YOU WIN");
+		}
+		else
+		{
+			print ("Go Heal!");
+		}
+	}
+}
